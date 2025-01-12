21383117d56324bbf7fdc52c3c8e78e9624dd9c8
agexxxecute <167965583+agexxxecute@users.noreply.github.com>
2025-01-12 16:00:49 +0400

    Javadoc update

--- src/main/java/com/petproject/orderservice/OrderServiceApplication.java
+++ src/main/java/com/petproject/orderservice/OrderServiceApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Основной класс микросервиса для работы с заказами.
+ * Предоставляет функционал создания и поиска заказов.
+ *
+ * @author Egor Nazarev
+ */
 @SpringBootApplication
 public class OrderServiceApplication {
 
